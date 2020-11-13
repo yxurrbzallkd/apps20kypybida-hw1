cd7c7126ee8d254a05b7e09d86d3d7065197575c
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2020-11-13 20:18:06 +0200

    Add files via upload

---
+++ TempSummaryStatistics.java
@@ -0,0 +1,15 @@
+package ua.edu.ucu.tempseries;
+
+public class TempSummaryStatistics {
+    public double avgTemp;
+	public double devTemp;
+	public double minTemp;
+	public double maxTemp;
+
+	public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp){
+		this.avgTemp = avgTemp;
+		this.devTemp = devTemp;
+		this.minTemp = minTemp;
+		this.maxTemp = maxTemp;
+	}
+}
