4cce37d836a78128dffdebd603a5eb4301e05c73
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2020-11-13 23:45:14 +0200

    Add files via upload

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,11 +1,10 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    public double avgTemp;
-	public double devTemp;
-	public double minTemp;
-	public double maxTemp;
-
+    private final double avgTemp;
+	private final double devTemp;
+	private final double minTemp;
+	private final double maxTemp;
 
 	public TempSummaryStatistics(TemperatureSeriesAnalysis series){
 		this.avgTemp = series.average();
@@ -13,7 +12,6 @@ public class TempSummaryStatistics {
 		this.minTemp = series.min();
 		this.maxTemp = series.max();
 	}
-
 	public String toString(){
 		return "Average: "+this.avgTemp+" Deviation: "+this.devTemp+" Min: "+this.minTemp+" Max: "+this.maxTemp;
 	}
